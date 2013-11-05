09323bbdab2560d5397bf6c2cb33c9be0035a3d3
Daniel Bloemendal <d.bloemendal@gmail.com>
2013-11-05 04:57:40 -0500

    Revert "added FFT, vocoder (using FFT) and test routine"
    This reverts commit 7ac51d89c7f5613fd76f7b25e7f91c2c7184161c.

--- L4/src/user.h
+++ L4/src/user.h
@@ -12,11 +12,9 @@
 #include "defines.h"
 #include "debug.h"
 #include "ringbuffer.h"
-#include "stack.h"
 
 // Lab 4 includes
 #include "audio.h"
-#include "fft.h"
 #include "wave.h"
 #include "notes.h"
 
@@ -29,20 +27,7 @@
 RC SysCall( SysCallType type, uval32 arg0, uval32 arg1, uval32 arg2);
 void ThreadExit(void);
 RC U_VirtualSysCall(SysCallType type, uval32 arg0, uval32 arg1, uval32 arg2);
+
 void mymain(void);
-
-// Assert macro
-#ifdef ASSERT
-#undef ASSERT
-#endif
-#define ASSERT(expr) user_assert((expr),#expr,__FILE__,__LINE__)
-
-// User assert routine
-inline void user_assert(int success, const char* expr, const char* file, int line) {
-    if(!success) {
-        printk("Assertion '%s' failed at line %d of file %s!\n", expr, line, file);
-        SysCall(SYS_DESTROY, 0, 0, 0);
-    }
-}
 
 #endif
