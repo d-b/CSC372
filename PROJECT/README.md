b736d12becc031ec2abe6eb12aa6d6a449eac30a
Daniel Bloemendal <d.bloemendal@gmail.com>
2013-11-21 05:24:50 -0500

    project initials

---
+++ PROJECT/README.md
@@ -0,0 +1,4 @@
+Project
+======
+
+Over the air modem
---
+++ PROJECT/src/modem.cpp
@@ -0,0 +1,10 @@
+/*
+ * CSC372 - Project: Modem
+ *
+ * Daniel Bloemendal <d.bloemendal@gmail.com>
+ * David Lu <david-lu@hotmail.com>
+ */
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
