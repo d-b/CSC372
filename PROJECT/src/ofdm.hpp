1f7b78fa6ae5fe912bb1e8cff719618336b21f60
Daniel Bloemendal <d.bloemendal@gmail.com>
2013-11-27 11:20:07 -0500

    updates to OFDM

--- PROJECT/src/ofdm.hpp
+++ PROJECT/src/ofdm.hpp
@@ -7,86 +7,43 @@
 
 #pragma once
 
-class Bitmap {
-    // Types
-    typedef uint32_t field_t;
+namespace modem
+{
+    class ofdm {
+    public:        
+        typedef struct {
+            // Base parameters
+            uint16_t rate;
+            uint16_t points;
+            uint16_t carrier;
+            uint16_t bandwidth;
+            uint16_t symbols;
+            double   threshold;
 
-    // Internal fields
-    std::vector<std::bitset<sizeof(field_t)*8>> fields;
-    uint32_t total;
+            // Preamble
+            uint16_t preamble_length;
+            uint16_t preamble_frequency;
 
-    // Resize internal bitmap
-    void resize(uint32_t bits);
+            // Cyclic prefix
+            uint16_t cyclicprefix_length;
+        } parameters_t;
 
-public:
-    // Types
-    typedef uint32_t bit_t;
+    private:
+        // OFDM parameters
+        parameters_t parameters;
 
-    // Constructor & copy constructor
-    Bitmap() {}
-    Bitmap(const Bitmap& bitmap)
-        : fields(bitmap.fields) {};
+        // Equalization parameters
+        spectrum equalization_freqresponse;
 
-    // Methods
-    void     set(bit_t index);
-    void     reset(bit_t index);
-    bool     test(bit_t index) const;
-    bit_t    ffs(void) const;
-    uint32_t count(void) const;
+        // Synchronization and interference mitigation mechanisms
+        void insert_preamble(signal& sig, uint16_t length);
+        void insert_cyclicprefix(signal& sig, uint16_t length);
+        void insert_trainingsymbol(signal& sig);
 
-private:
-    constexpr static uint32_t bits(void) {
-        return sizeof(field_t)*8;}
-    inline uint32_t field(bit_t index) const {
-        return index/bits();}
-};
+        // Frame detection
+        bool frame_test(signal& sig, size_t* offset = NULL);
 
-class Spectrum {
-public:    
-    // Types
-    typedef Bitmap::bit_t subcarrier_t;
-    typedef double scale_t;
-    typedef std::unordered_map<subcarrier_t, scale_t> Scalemap;
-
-    // Constructor & copy constructor
-    Spectrum() {};
-    Spectrum(const Spectrum& spectrum)
-        : carriermap(spectrum.carriermap), scalemap(spectrum.scalemap) {};
-
-    // Methods
-    void add(subcarrier_t index, double scale);
-    void remove(subcarrier_t index);
-    bool test(subcarrier_t index) const;
-    double scale(subcarrier_t index) const;
-    uint32_t components(void) const;
-    Bitmap bitmap(void) const;
-
-private:
-    Bitmap carriermap;
-    Scalemap scalemap;
-};
-
-class OFDM {
-    // OFDM parameters
-    Spectrum spectrum;
-    const uint32_t fft_size;
-    const uint32_t guard_size;
-
-    // FFTW context
-    fftw_plan plan;
-    fftw_complex* input;
-    fftw_complex* output;
-
-public:
-    // OFDM instance with given spectrum, fft size and guard size
-    OFDM(const Spectrum& spectrum, uint32_t fft_size, uint32_t guard_size);
-    virtual ~OFDM();
-
-    // Spectrum control
-    Spectrum get_spectrum(void) const;
-    void     set_spectrum(const Spectrum& spectrum);
-
-    // Send and receive
-    void send(const std::vector<std::complex<double>>& frame);
-    void receive(const std::vector<std::complex<double>>& frame);
-};
+    public:
+        ofdm(parameters_t parameters);
+    };
+}
